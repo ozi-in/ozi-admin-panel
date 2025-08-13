41eee1ed0e1543aa44222aa2753fbacfffde7c74
Harpreet <harpreet.kaur@digimantra.com>
2025-08-13 11:15:34 +0530

    enablefirebas

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,13 +2,13 @@ importScripts('https://www.gstatic.com/firebasejs/8.3.2/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.3.2/firebase-messaging.js');
 
 firebase.initializeApp({
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+    apiKey: "AIzaSyDnPPwp67SJExYPX-ZDFtbZAFhm64CJxuo",
+    authDomain: "ozi-technologies-99593.firebaseapp.com",
+    projectId: "ozi-technologies-99593",
+    storageBucket: "ozi-technologies-99593.firebasestorage.app",
+    messagingSenderId: "436594923301",
+    appId: "1:436594923301:android:e105e4164ed9fa03962ccc",
+    measurementId: "G-4NSPLT0604"
 });
 
 const messaging = firebase.messaging();
